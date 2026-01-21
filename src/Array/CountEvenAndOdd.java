27044bcfb0717638f41243321924c92e61d06b5e
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-01-21 14:18:35 +0530

    array code

---
+++ src/Array/CountEvenAndOdd.java
@@ -0,0 +1,20 @@
+package Array;
+
+public class CountEvenAndOdd {
+    public static void main(String[] args) {
+        int arr []={0,1,2,3,4,5,6};
+        int even =0;
+        int odd =0;
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]%2==0){
+                even++;
+            } else if (arr[i]==0) {
+                even++;
+            } else{
+                odd++;
+            }
+        }
+        System.out.println(even+"--"+odd);
+    }
+}
---
+++ src/Array/DiffBetnMaxAndMin.java
@@ -0,0 +1,22 @@
+package Array;
+//Write a Java program to get the difference between the largest and smallest values in an array of integers. The array must have a length of at least 1.
+public class DiffBetnMaxAndMin {
+
+    public static void main(String[] args) {
+
+        int arr[]={1,2,3,4,5,6};
+        int small=arr[0];
+        int large=arr[0];
+        int diff =0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>large){
+                large=arr[i];
+            } else if (arr[i]<small) {
+                small=arr[i];
+            }
+            diff = large-small;
+        }
+        System.out.println(diff);
+
+    }
+}
