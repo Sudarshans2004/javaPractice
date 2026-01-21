8646f248e69c6505ed07ed02c11432294cca3852
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-01-21 19:22:31 +0530

    array code

---
+++ src/Abstraction/AbstractClassRule3.java
@@ -0,0 +1,14 @@
+package Abstraction;
+
+ abstract class Test {
+// We cannot create object of abstract class
+    abstract void display();
+
+}
+public class AbstractClassRule3 {
+    public static void main(String[] args) {
+//        Test Obj = new Test() We cannot create obj here ERROR
+
+
+    }
+}
---
+++ src/Abstraction/AbstractionRule1.java
@@ -0,0 +1,13 @@
+package Abstraction;
+
+public abstract class AbstractionRule1 {
+    //    rule1 = it is not mandotary to have abstract method in abstract class
+    void class1(){
+        System.out.println("concreate class 2");
+    }
+    void class2(){
+        System.out.println("concreate class 2");
+    }
+    abstract void class3();  //abstract class
+
+}
