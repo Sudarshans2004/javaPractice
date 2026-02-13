aff4de072065091e85ab36df199265966fe4c7ac
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-02-13 16:00:31 +0530

    Method Overriding

--- src/methodoverriding/VariableOverriding.java
+++ src/methodoverriding/VariableOverriding.java
@@ -1,4 +1,27 @@
 package methodoverriding;
 
+// Variable resolution is depends on jvm based of refrence types .
+// Overriding concept is applcable for methods mot for variable
+
 public class VariableOverriding {
+
+    // if we create variable static or non static the result will be same
+
+    int x = 10;
+}
+class child extends VariableOverriding{
+    int x =100;
+
+}
+class d {
+    public static void main(String[] args) {
+        VariableOverriding v = new VariableOverriding();
+        System.out.println(v.x);
+
+        child c = new child();
+        System.out.println(c.x);
+
+        VariableOverriding v1 = new VariableOverriding();
+        System.out.println(v1.x);
+    }
 }
