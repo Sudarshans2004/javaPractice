e9e317eaa04a8c960d5fe6f3c0880fb9d6169329
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-02-14 16:06:25 +0530

    methodoverloading

--- src/methodoverriding/MethodOverridingRule9.java
+++ src/methodoverriding/MethodOverridingRule9.java
@@ -3,9 +3,15 @@ package methodoverriding;
 // will help to stop availability of parent method implementation to next levelchild
 public class MethodOverridingRule9 {
     public void m1 (){
-
+        System.out.println("stop availability");
     }
 }
 abstract class chiiild extends MethodOverridingRule9{
     public abstract void m1();
+}
+class l {
+    public static void main(String[] args) {
+        chiild c = new chiild();
+        c.m1();
+    }
 }
