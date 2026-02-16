3339fa549af4121b62430323a5533c4a514e1041
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-02-16 19:28:14 +0530

    doubleequalsanddotequals

---
+++ src/statickeyword/Static.java
@@ -0,0 +1,11 @@
+package statickeyword;
+
+public class Static {
+    static{
+        main (new String[] {});
+    }
+
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+    }
+}
