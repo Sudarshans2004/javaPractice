f2d3905a0c7b32ab9c4141cbae0315a82c2a21ff
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-02-19 14:45:56 +0530

    polymorphism practice questins

---
+++ src/polymorphism/Ques2.java
@@ -0,0 +1,18 @@
+package polymorphism;
+
+public class Ques2 {
+//    3) In the below class, is ‘method’ overloaded or duplicated?
+//it is method dupliacte
+    {
+//        void method ( int ...a)
+        {
+            System.out.println(1);
+        }
+
+//        void method ( int[] a)
+        {
+            System.out.println(2);
+        }
+    }
+}
+
