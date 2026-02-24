a4c95246c9eedc897aa054fa31a9a32a0c9db2a9
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-02-24 14:45:01 +0530

    Array practice

---
+++ src/array/Subset.java
@@ -0,0 +1,11 @@
+package array;
+//.print subset of array
+public class Subset {
+    public static void main(String[] args) {
+        int arr []= {1,2,3};
+        for(int i=0 ;i<arr.length;i++ ){
+
+        }
+    }
+
+}
