e405fae4c3a83cb1c9510429b8bbda5cdfaebf9d
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-02-27 13:13:57 +0530

    Array practice

---
+++ src/array/ClimbingStairs.java
@@ -0,0 +1,21 @@
+package array;
+
+public class ClimbingStairs {
+        public static int climbStairs(int n) {
+            if(n<=1){
+                return 1;
+            }
+            int a=1,b=1;
+            for(int i=2;i<=n;i++){
+                int c =a+b;
+                a=b;
+                b=c;
+
+            }
+            return b;
+        }
+
+    public static void main(String[] args) {
+        System.out.println(climbStairs(4));
+    }
+}
