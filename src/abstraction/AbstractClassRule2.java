3ac9c26ab0d9dc38ff30c3c639899deeda114973
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-02-28 14:02:44 +0530

    Array practice

--- src/abstraction/AbstractClassRule2.java
+++ src/abstraction/AbstractClassRule2.java
@@ -4,7 +4,8 @@ public class AbstractClassRule2 {
     //if we define single abstract method in a class then that class must be abstract class
 
 //    abstract void class1();   error
-    public void class2(){
+    public void class2()
+    {
         System.out.println("class must be asbtract for abstract method ");
     }
 
