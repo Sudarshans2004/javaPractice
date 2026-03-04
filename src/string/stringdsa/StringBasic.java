09d2ff91a49f8c6c4421856aea1d232b173bf6b6
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-03-04 19:15:12 +0530

    String Basics

---
+++ src/string/stringdsa/StringBasic.java
@@ -0,0 +1,11 @@
+package string.stringdsa;
+
+public class StringBasic {
+    public static void main(String[] args) {
+        String s =  new String("Durga");
+       String s1=  s.concat("Solutions");
+        System.out.println(s1);
+        Integer I = Integer.valueOf("101", 4);
+        System.out.println(I);   // 17
+    }
+}
