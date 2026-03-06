4f2caa935f9296b2c7dc04875a93ea6df7089263
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-03-06 18:27:12 +0530

    String Basics

--- src/string/stringdsa/StringBasic.java
+++ src/string/stringdsa/StringBasic.java
@@ -2,10 +2,13 @@ package string.stringdsa;
 
 public class StringBasic {
     public static void main(String[] args) {
-        String s =  new String("Durga");
-       String s1=  s.concat("Solutions");
-        System.out.println(s1);
-        Integer I = Integer.valueOf("101", 4);
-        System.out.println(I);   // 17
+//        String s =  new String("Durga");
+//       String s1=  s.concat("Solutions");
+//        System.out.println(s1);
+//        Integer I = Integer.valueOf("101", 4);
+//        System.out.println(I);   // 17
+        Character c = Character.valueOf('a');
+        System.out.println(c);
+
     }
 }
