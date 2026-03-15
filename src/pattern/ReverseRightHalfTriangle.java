5fb2048886294dc4e83d270c6e155a5186cf3337
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-03-15 17:26:23 +0530

    Pattern Questions Practice

---
+++ src/pattern/ReverseRightHalfTriangle.java
@@ -0,0 +1,30 @@
+package pattern;
+
+public class ReverseRightHalfTriangle {
+    public static void main(String[] args) {
+        int n =5;
+        for(int i =1;i<=n;i++){
+
+            for(int j =1;j<=n;j++){
+                if(j>=i){
+                    System.out.print("* ");
+                }
+
+            }
+            System.out.println();
+        }
+            for(int i =2;i<=n;i++){
+
+                for(int j =1;j<=n;j++){
+                    if(j<=i){
+                        System.out.print("* ");
+                    }
+
+                }
+                System.out.println();
+            }
+
+
+
+    }
+}
