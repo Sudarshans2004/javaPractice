3e3ac16b342c4a9c8eb5da85cef2c1ff82d29e62
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-03-15 17:26:09 +0530

    Pattern Questions Practice

---
+++ src/pattern/NumberTriangularPat.java
@@ -0,0 +1,21 @@
+package pattern;
+
+public class NumberTriangularPat {
+    public static void main(String[] args) {
+        int n =5;
+        for(int i =1;i<=n;i++){
+
+            for(int j =1;j<=n;j++){
+
+                if(i+j<n+1){
+                    System.out.print(" ");
+                }else{
+                    System.out.print(i+" ");
+                }
+
+            }
+            System.out.println();
+        }
+
+    }
+}
