ce645991b24e705b9e15a36efc04ec7bf563137f
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-03-25 12:40:24 +0530

    Innerclass practice

--- src/innerclass/regularinner/InnerClassFromInstance.java
+++ src/innerclass/regularinner/InnerClassFromInstance.java
@@ -1,19 +1,18 @@
 package innerclass.regularinner;
 
 public class InnerClassFromInstance {
+      class inner {
+          public  void display(){
+              System.out.println("Display");
+          }
+      }
+      public void m10 (){
+          inner i = new inner();
+          i.display();
+      }
 
-    class Inner{
-       public void mm2(){
-           System.out.println("inner class from instance");
-       }
-
-    }
-    public void mm1(){
-        Inner i = new Inner();
-        i.mm2();
-    }
     public static void main(String[] args) {
         InnerClassFromInstance i = new InnerClassFromInstance();
-        i.mm1();
+        i.m10();
     }
 }
