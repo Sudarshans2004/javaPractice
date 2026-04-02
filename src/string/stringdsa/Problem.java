1c3296885d7ebc90fe568eb9330b63f285619062
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-04-02 18:34:07 +0530

    String DSA

---
+++ src/string/stringdsa/Problem.java
@@ -0,0 +1,9 @@
+package string.stringdsa;
+
+public class Problem {
+    public static void main(String[] args) {
+        String s = "nayan";
+        StringBuilder s1 = new StringBuilder("nayan");
+        System.out.println(s.equals(s1));
+    }
+}
