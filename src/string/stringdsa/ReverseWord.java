e05c1aff8efda6bf847a309f0c784471e8601df2
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-04-02 11:05:59 +0530

    String DSA

--- src/string/stringdsa/ReverseWord.java
+++ src/string/stringdsa/ReverseWord.java
@@ -2,11 +2,14 @@ package string.stringdsa;
 
 public class ReverseWord {
     public static void main(String[] args) {
-        String s = "siddhesh bagged highest package";
-        String [] s2 =s.split(" ");
-        String s1 [] =new String[s2.length];
+//        String s = "siddhesh bagged highest package";
+//        String [] s2 =s.split(" ");
+//        String s1 [] =new String[s2.length];
+        String s = "Sudarshan.Shinde";
+        String s2 [] = s.split(".");
+         int uu = s2.length;
+        System.out.println(uu);
 
-        System.out.println(s2[0]);
         for(int i=s2.length-1;i>=0;i--){
             System.out.print(s2[i]+" ");
         }
