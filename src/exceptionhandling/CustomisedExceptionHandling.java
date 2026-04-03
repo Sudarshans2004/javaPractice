93f08df7f6d552c7550d218c41ba269d5b600f5f
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-04-03 07:58:07 +0530

    Exception Handling

--- src/exceptionhandling/CustomisedExceptionHandling.java
+++ src/exceptionhandling/CustomisedExceptionHandling.java
@@ -1,24 +1,37 @@
 package exceptionhandling;
 
-class tooOldException extends RuntimeException{
-    tooOldException(String s ){
+class TooOldException extends RuntimeException {
+    TooOldException(String s) {
         super(s);
     }
 }
-class tooYoungException extends RuntimeException{
-    tooYoungException(String s){
+
+class TooYoungException extends RuntimeException {
+    TooYoungException(String s) {
         super(s);
     }
 }
+
 public class CustomisedExceptionHandling {
     public static void main(String[] args) {
-        int age =Integer.parseInt(args[0]);
-        if(age>60){
-            throw new tooOldException("Your age have crossed marriage age");
-        } else if (age<18) {
-            throw new tooYoungException("Plz wait some yrs you will get match soon");
-        }else{
-            System.out.println("you will get match details soon");
+
+        try {
+            int age = Integer.parseInt(args[0]);
+
+            if (age > 60) {
+                throw new TooOldException("Your age has crossed marriage age");
+            } else if (age < 18) {
+                throw new TooYoungException("Please wait some years, you will get match soon");
+            } else {
+                System.out.println("You will get match details soon");
+            }
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Please provide age as command line argument");
+        } catch (NumberFormatException e) {
+            System.out.println("Please enter valid number");
+        } catch (TooOldException | TooYoungException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
