57d5e09b586317611243db90cb7609b5f4708c40
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-04-04 12:01:25 +0530

    StringDsa

--- src/multithreading/ExtendingThreadClass.java
+++ src/multithreading/ExtendingThreadClass.java
@@ -1,5 +1,13 @@
 package multithreading;
 
-public class ExtendingThreadClass {
+public class ExtendingThreadClass extends Thread {
+    public void run(){
+        System.out.println("run");
+    }
 
+    public static void main(String[] args) {
+        ExtendingThreadClass e = new ExtendingThreadClass();
+        Thread t = new Thread(e);
+        t.start();
+    }
 }
