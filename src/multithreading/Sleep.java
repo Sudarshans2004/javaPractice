9adddf829505f2bd481f48de6e7d6e036c86173f
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-04-04 13:57:38 +0530

    Multithreading

---
+++ src/multithreading/Sleep.java
@@ -0,0 +1,11 @@
+package multithreading;
+
+public class Sleep {
+    public static void main(String[] args) throws InterruptedException{
+        for(int i =0;i<10;i++){
+            System.out.println("Shinde");
+            Thread.sleep(100);
+        }
+
+    }
+}
