d171adf99129d1a2b5dbfc95bc590a7b848e7394
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-04-04 13:57:49 +0530

    Multithreading

---
+++ src/multithreading/ThreadPriority.java
@@ -0,0 +1,10 @@
+package multithreading;
+
+public class ThreadPriority {
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getPriority());
+        Thread.currentThread().setPriority(9);
+        System.out.println(Thread.currentThread().getPriority());
+
+    }
+}
