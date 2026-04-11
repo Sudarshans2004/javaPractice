bff24aec46d676bcaa0a8eb076e0dda0e5c94bee
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-04-11 14:49:12 +0530

    Multithreading

---
+++ src/multithreading/core.java
@@ -0,0 +1,8 @@
+package multithreading;
+
+public class core {
+    public static void main(String[] args) {
+        int no = Runtime.getRuntime().availableProcessors();
+        System.out.println(no);
+    }
+}
