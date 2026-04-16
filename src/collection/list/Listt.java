1b50bd3a86a2e329d7587eb60ded66123f4c3f0e
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-04-16 15:53:02 +0530

    Multithreading

---
+++ src/collection/list/Listt.java
@@ -0,0 +1,21 @@
+package collection.list;
+
+import java.util.ArrayList;
+
+public class Listt {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> l = new ArrayList<>();
+//        ArrayList l1 = new ArrayList();
+        for(int i = 0; i < 10; i++){
+            l.add(i);
+        }
+
+        System.out.println(l);
+        l.remove(2);
+        l.add(5,null);
+        l.add(3,12);
+        System.out.println(l.get(9));
+        System.out.println(l);
+    }
+}
