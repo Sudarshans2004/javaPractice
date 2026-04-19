13e6aa71332a95a56696b224d0fe9514c77603be
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-04-19 14:04:26 +0530

    Multithreading

--- src/collection/list/Vectorr.java
+++ src/collection/list/Vectorr.java
@@ -5,6 +5,12 @@ import java.util.Vector;
 public class Vectorr {
     public static void main(String[] args) {
         Vector v = new Vector<>();
+        Vector v1 = new Vector(10);
+        Vector v2 = new Vector(10,10);
+//        Vector v3 = new Vector(Collection c );
+
+
+
         System.out.println( v.capacity());
         for(int i =0;i<10;i++){
             v.addElement(i);
@@ -12,6 +18,7 @@ public class Vectorr {
         System.out.println(v.capacity());
         v.remove(5);
         System.out.println(v);
+        System.out.println(v.capacity());
         v.clear();
         System.out.println(v);
     }
