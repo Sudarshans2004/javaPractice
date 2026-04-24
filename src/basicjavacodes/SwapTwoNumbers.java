0a09c092409ff60180a4ebdd121d4ab09225e22f
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-04-24 20:03:34 +0530

    Multithreading

---
+++ src/basicjavacodes/SwapTwoNumbers.java
@@ -0,0 +1,10 @@
+package basicjavacodes;
+
+public class SwapTwoNumbers {
+    public static void main(String[] args) {
+        int a =5,b=7;
+        a=a+b;
+        b=a-b;
+        b=a-b;
+    }
+}
