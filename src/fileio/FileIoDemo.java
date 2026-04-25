7ed8d89f5916adfbf6ef20b619e588d6d6144667
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-04-25 14:24:39 +0530

    Basic Program coding practice

---
+++ src/fileio/FileIoDemo.java
@@ -0,0 +1,10 @@
+package fileio;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileIoDemo {
+    public static void main(String[] args) throws IOException {
+//        File f = new File();
+    }
+}
