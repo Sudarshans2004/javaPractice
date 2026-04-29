ed864e1023462f75ff2471cdc56399bf8f4a1fd5
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-04-29 17:45:36 +0530

    Basic Program coding practice

--- src/string/stringdsa/ReverseWord.java
+++ src/string/stringdsa/ReverseWord.java
@@ -5,8 +5,8 @@ public class ReverseWord {
 //        String s = "siddhesh bagged highest package";
 //        String [] s2 =s.split(" ");
 //        String s1 [] =new String[s2.length];
-        String s = "Sudarshan.Shinde";
-        String s2 [] = s.split(".");
+        String s = "Sudarshan Shinde";
+        String s2 [] = s.split(" ");
          int uu = s2.length;
         System.out.println(uu);
 
