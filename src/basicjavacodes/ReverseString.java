7c23b97dce11ec95ac61f5a6e3aa9ba2900fb0a2
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-04-29 17:45:53 +0530

    Basic Program coding practice

---
+++ src/basicjavacodes/ReverseString.java
@@ -0,0 +1,15 @@
+package basicjavacodes;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String s1 = "Sudarshan";
+        String reverse = "";
+        for(int i = s1.length()-1;i>=0;i--){
+            reverse=reverse+s1.charAt(i);
+        }
+
+        System.out.println(reverse);
+        StringBuilder sb = new StringBuilder(s1);
+
+    }
+}
