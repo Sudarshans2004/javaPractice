bacf98aba8c3670cddd1346858744a700b7de477
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-04-30 18:27:18 +0530

    Basic Program coding practice

---
+++ src/basicjavacodes/FindFrequency.java
@@ -0,0 +1,26 @@
+package basicjavacodes;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindFrequency {
+    public static void main(String[] args) {
+        int [] arr = {1,2,4,5,6,2,1,4,5,1,7};
+        int result = -1;
+        HashMap <Integer,Integer> h = new HashMap<>();
+        for(int i = 0; i< arr.length;i++){
+                h.put(arr[i], h.getOrDefault(arr[i],0)+1);
+        }
+        for (Map.Entry<Integer, Integer> entry : h.entrySet()) {
+           int key = -1;
+           int value = 0;
+           key = entry.getKey();
+           value = entry.getValue();
+           if(key == value){
+                result = Math.max(key ,result);
+           }
+        }
+        System.out.println(result);
+
+    }
+}
