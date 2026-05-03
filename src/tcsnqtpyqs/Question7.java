cc786d425197a5e4164922ac41f1f4035a8d4757
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-05-03 19:28:00 +0530

    Program coding practice

--- src/tcsnqtpyqs/Question7.java
+++ src/tcsnqtpyqs/Question7.java
@@ -1,5 +1,15 @@
 package tcsnqtpyqs;
-
+//N horses are listed in order. Bob wants to bet on a continuous sequence of horses, spending less than K units total.
+// Find the maximum length of such a sequence.
+//
+//Examples:
+//
+//N=10, K=100, prices=30 40 50 20 20 10 90 10 10 10 → Output: 3
+//N=10, K=100, prices=10 90 80 20 90 60 40 60 70 75 → Output: 1
+//Constraints:
+//
+//2 ≤ N ≤ 10⁵
+//1 ≤ K ≤ 10⁹
 public class Question7 {
     public static void main(String[] args) {
         int[] prices = {10, 90, 80, 20, 90, 60, 40, 60, 70, 75};
