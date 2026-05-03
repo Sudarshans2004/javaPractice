0fb648c86b131f9968392cc3fc35d8c5222703f6
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-05-03 19:19:53 +0530

    Program coding practice

--- src/basicjavacodes/FindFrequency.java
+++ src/basicjavacodes/FindFrequency.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class FindFrequency {
     public static void main(String[] args) {
-        int [] arr = {1,2,4,5,6,2,1,4,5,1,7};
+        int [] arr = {1,2,4,5,6,2,1,4,5,1,7,5,5,5};
         int result = -1;
         HashMap <Integer,Integer> h = new HashMap<>();
         for(int i = 0; i< arr.length;i++){
