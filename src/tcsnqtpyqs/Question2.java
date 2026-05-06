9dce21719d1efefcb5770011118f631b14531abd
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-05-06 15:43:39 +0530

    Program coding practice

--- src/tcsnqtpyqs/Question2.java
+++ src/tcsnqtpyqs/Question2.java
@@ -1,13 +1,9 @@
 package tcsnqtpyqs;
-
 //Consider that you are managing an inventory system in a warehouse. Some products are packaged in bulk quantities of 10,
 // and these products need to be sent to a different warehouse.
-//
 //Your task is to rearrange the inventory list such that all products whose quantities are multiples of 10 are moved to
 // the end, while ensuring that the order of the remaining products is not disturbed.
-//
 //Problem Statement
-//
 //You are given an array of integers representing product quantities. Rearrange the array so that all multiples of 10
 // appear at the end of the array, while keeping the relative order of all other elements unchanged.
 
