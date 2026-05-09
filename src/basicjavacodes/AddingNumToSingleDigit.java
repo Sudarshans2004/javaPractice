6177c9baee122a414c39f551fd4c046da1057c29
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-05-09 17:34:09 +0530

    DSA practice

---
+++ src/basicjavacodes/AddingNumToSingleDigit.java
@@ -0,0 +1,24 @@
+package basicjavacodes;
+
+public class AddingNumToSingleDigit {
+    public static void main(String[] args) {
+        int num =0;
+        while (num >= 10) {
+
+            int result = 0;
+
+            while (num > 0) {
+
+                int digit = num % 10;
+                result += digit;
+                num /= 10;
+            }
+
+            num = result;
+        }
+
+        System.out.println(num); ;
+    }
+
+  String s = "Sudarshan shinde";
+}
