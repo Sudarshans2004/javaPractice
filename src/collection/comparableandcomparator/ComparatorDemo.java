3b36c21bf701ee484b5cbd3e4db60c8103bcfdf6
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-06-03 13:01:50 +0530

    Comparator prac

--- src/collection/comparableandcomparator/ComparatorDemo.java
+++ src/collection/comparableandcomparator/ComparatorDemo.java
@@ -19,7 +19,6 @@ class MyComparator implements Comparator{
     public int compare(Object o1, Object o2) {
         Integer i1 = (Integer) o1;
         Integer i2 = (Integer) o2;
-
 //        if(i1 > i2){
 //          return +1;
 //        } else if (i1< i2) {
