f84a358ecdd31a2673da967faf67b933b7d3339a
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-06-16 18:32:31 +0530

    Stream Api

---
+++ src/java8/streamapi/BasicCode1.java
@@ -0,0 +1,19 @@
+package java8.streamapi;
+
+import java.util.ArrayList;
+
+public class BasicCode1 {
+    public static void main(String[] args) {
+        ArrayList <Integer>l = new ArrayList<Integer>();
+        l.add(10);
+        l.add(20);
+        l.add(70);
+        l.add(15);
+        l.add(25);
+        Integer [] i = l.stream().toArray(Integer[] ::new);
+
+        for(Integer i1 : i){
+            System.out.println(i1);
+        }
+    }
+}
