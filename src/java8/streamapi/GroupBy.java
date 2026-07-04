3e8d8e6c6b37478b18d11e2f84d48204688e725a
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-07-04 13:55:38 +0530

    Stream Api

--- src/java8/streamapi/GroupBy.java
+++ src/java8/streamapi/GroupBy.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class groupBy {
+public class GroupBy {
     public static void main(String[] args) {
         List<String> names = Arrays.asList(
                 "Rajesh", "Suresh", "Amit", "Rohan", "Mahesh"
