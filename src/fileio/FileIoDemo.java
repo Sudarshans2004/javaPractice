e4864cea4cec87542e5aa46ad341a4384a3f70f2
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-07-19 17:25:03 +0530

    Stream Api

--- src/fileio/FileIoDemo.java
+++ src/fileio/FileIoDemo.java
@@ -6,5 +6,8 @@ import java.io.IOException;
 public class FileIoDemo {
     public static void main(String[] args) throws IOException {
 //        File f = new File();
+        int no = Runtime.getRuntime().availableProcessors();
+        System.out.println(no);
+
     }
 }
