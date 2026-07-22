931e6a550d020923187ba8dfa736db4446cbac36
sudarshan shinde <sudarshanshinde033@gmail.com>
2026-07-22 14:56:23 +0530

    array dsa

--- src/array/ArrayListToArray.java
+++ src/array/ArrayListToArray.java
@@ -1,23 +1,34 @@
 package array;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 //Write a Java program to convert an ArrayList to an array .
 public class ArrayListToArray {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
         list.add(2);
-        list.add(4);
+        list.add(10);
         list.add(6);
         list.add(8);
-
+        Collections.sort(list);
 
         int arr[] = new int [list.size()];
             int i=0;
             while( i<list.size()){
                 arr[i]= list.get(i);
-                System.out.println(arr[i]);
                 i++;
             }
+        System.out.println(Arrays.toString(arr));
+            int x = arr.length;
+            int y;
+            String s= "ncowne";
+            s.length();
+
+        Arrays.sort(arr);
+        System.out.println(arr);
+
 
     }
 }
