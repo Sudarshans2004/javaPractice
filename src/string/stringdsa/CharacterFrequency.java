593512b9e10bb438b10627783444b66b24c1917c
Sudarshan <sudarshanshinde033@gmail.com>
2026-08-26 17:03:06 +0530

    array dsa

---
+++ src/string/stringdsa/CharacterFrequency.java
@@ -0,0 +1,18 @@
+package string.stringdsa;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class CharacterFrequency {
+    public static void main(String[] args) {
+        String s = "sudarshan";
+        Map<Character,Integer> freq = new LinkedHashMap<>();
+        for(char c :s.toCharArray()){
+            freq.put(c ,freq.getOrDefault(c,0)+1);
+        }
+
+        System.out.println(freq);
+    }
+
+}
