9ba8e227ce78e07484bcea09ae81b1c1589b9bd7
Sudarshan <sudarshanshinde033@gmail.com>
2026-09-26 19:19:05 +0530

    matrix practice

---
+++ src/matrices/Transpose.java
@@ -0,0 +1,24 @@
+package matrices;
+
+public class Transpose {
+    public static int[][] transpose(int[][] matrix) {
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int [][] transMatrx = new int [n][m];
+
+        for(int i=0;i<m;i++){
+
+            for(int j=0;j<n;j++){
+
+                transMatrx[j][i]=matrix[i][j];
+            }
+        }
+        return transMatrx;
+    }
+    public static void main(String[] args) {
+        int[][] arr = {{1,2,3}, {4,5,6}, {7,8,9}};
+        transpose(arr);
+    }
+}
