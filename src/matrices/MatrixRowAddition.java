61d55986ac83984d974412454726a22e531e9004
Sudarshan <sudarshanshinde033@gmail.com>
2026-09-26 17:40:52 +0530

    matrix practice

--- src/matrices/MatrixRowAddition.java
+++ src/matrices/MatrixRowAddition.java
@@ -1,15 +1,16 @@
 package matrices;
 
-public class MatrixAddition {
+public class MatrixRowAddition {
     public static void main(String[] args) {
         int m=3,n=3;
         int sum=0;
         int arr[][] = {{1,2,3},{4,5,6 },{7,8,9}};
         for(int i=0;i<n;i++){
+             sum =0;
             for(int j=0;j<m;j++){
                 sum+=arr[i][j];
             }
+            System.out.println(sum);
         }
-        System.out.println(sum);
     }
 }
